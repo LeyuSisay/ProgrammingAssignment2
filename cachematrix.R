2985c139bd4d24a732d63437c7e31b7d7bb85eb3
Leyu Sisay <Leyu_s@hotmail.com>
2017-05-11 16:28:16 +0300

    Implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+
+## Uses attributes to store the closure functions: getinversed, setinversed.
+##getinversed: will make sure if there are any changes and invalidates the cache if so and returns the stored value
+##setinversed: will set cache value in the captured variable
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  original <- x
+  
+  attr(x,'getinversed') <- function(nx){
+    if(!identical(original,nx)){
+      inv <<- NULL
+      original <<- nx
+    }
+    inv
+  }
+  attr(x,'setinversed') <- function(val){
+    inv <<- val
+  }
+  x
 }
 
-
-## Write a short comment describing this function
-
+## Resolves the cache and if cache is null or invalidated, will compute the inverse and set the matrix inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversed <- attr(x,'getinversed')(x)
+  
+  if(!is.null(inversed)){
+    message("getting cached data")
+    return(inversed)
+  }
+  inversed <- solve(x)
+  attr(x,'setinversed')(inversed)
+  inversed
 }
